c73b19218b6dece3bf7a3774aafd36a9924fe2ff
ifandelse <jim@ifandelse.com>
2014-01-14 23:51:35 -0500

    Updated typo in Regex

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /*global require, module, Buffer */
 
-var evs = require('event-stream');
-var g_util = require('gulp-util');
-var js_hints  = /([\/][\*][\s*]jshint\s.+[\*\/])/g;
-var js_global = /([\/][\*][\s*]global\s.+[\*\/])/g;
+var evs = require("event-stream");
+var g_util = require("gulp-util");
+var js_hints  = /([\/][\*][\s]*jshint\s.+[\*\/])/g;
+var js_global = /([\/][\*][\s]*global\s.+[\*\/])/g;
 
 module.exports = function () {
 
     function processImports(file) {
         var contents;
 
-        // I'm no stream guru yet - so I have no idea if this can be done.....yet....
+        // I"m no stream guru yet - so I have no idea if this can be done.....yet....
         if (file.isStream()) {
-            this.emit('error', new g_util.PluginError('gulp-hint-not', 'Yikes - sorry about this, but streams are not supported yet.'));
+            this.emit("error", new g_util.PluginError("gulp-hint-not", "Yikes - sorry about this, but streams are not supported yet."));
         }
 
         if (file.isBuffer()) {
@@ -22,7 +22,7 @@ module.exports = function () {
             file.contents = new Buffer(contents);
         }
 
-        this.emit('data', file);
+        this.emit("data", file);
     }
 
     return evs.through(processImports);
